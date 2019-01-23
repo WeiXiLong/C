3b74ab6a85b85bbfcb441ff1416afd2db428c85c
WeiXiLong <438545362@qq.com>
2019-01-23 21:14:25 +0800

    1_23

---
+++ 2019_1_23/2019_1_23/main.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int i = 100;
+//	int j = 0;
+//	while (i <= 200) {
+//		for (j = 2; j < i; j++) {
+//			if (i % j == 0) {
+//				i++;
+//				break;
+//			}
+//		}
+//		if (j == i) {
+//			i++;
+//			printf("%d ",j);
+//		}
+//
+//	}
+//	system("pause");
+//	return 0;
+//}
