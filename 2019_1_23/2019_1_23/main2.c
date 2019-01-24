89a739de5e7597247ffe971f78c684594b7d9e8e
WeiXiLong <438545362@qq.com>
2019-01-24 10:43:46 +0800

    1_24

---
+++ 2019_1_23/2019_1_23/main2.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int j = 0;
+//	for (int i = 1; i <= 9; ++i) {
+//		for (j = 1; j <= i; ++j) {
+//			printf("%d * %d = %2d  ", i, j, i*j);
+//		}
+//		if (j > i) {
+//			printf("\n");
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
