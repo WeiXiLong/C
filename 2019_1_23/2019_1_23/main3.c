d710de5ccb36fecc53edbe3e1c7278820194ada5
WeiXiLong <438545362@qq.com>
2019-01-25 09:07:51 +0800

    1_25

---
+++ 2019_1_23/2019_1_23/main3.c
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1000; i <= 2000; ++i) {
+//		if (i % 4 == 0 && i % 100 != 0) {
+//			printf("%d  ",i);
+//			++count;
+//		}
+//		else if (i % 400 == 0) {
+//			printf("%d  ",i);
+//			++count;
+//		}
+//		if (count % 15 == 0 && count != 0) {
+//			printf("\n");
+//		}
+//	}
+//	printf("\n%d \n",count);
+//	system("pause");
+//	return 0;
+//}
