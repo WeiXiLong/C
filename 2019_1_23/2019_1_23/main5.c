694bdcefdc4975a015cf8c0e344cc86b2291bd11
WeiXiLong <438545362@qq.com>
2019-01-27 10:53:42 +0800

    1_27

---
+++ 2019_1_23/2019_1_23/main5.c
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("%d %d\n",a,b);
+//	system("pause");
+//	return 0;
+//}
