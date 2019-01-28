f8eb6cc3787cedfb71e6f1f778ff7809d98c71e7
WeiXiLong <438545362@qq.com>
2019-01-28 08:42:01 +0800

    1_28

---
+++ 2019_1_23/2019_1_23/main6.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int arr[10] = { 5,8,12,1,3,5,9,6,8,15 };
+//	while (1) {
+//		int i = 0;
+//		int con = arr[0];
+//		for (; i < 9; ++i) {
+//			if (arr[i] < arr[i + 1]) {
+//				con = arr[i + 1];
+//			}
+//		}
+//		printf("The max is %d \n",con);
+//		break;
+//	}
+//	system("pause");
+//	return 0;
+//}
