b1faa7cac22e6c999219e0d6b9778cd3d54a7466
WeiXiLong <438545362@qq.com>
2019-02-01 10:19:38 +0800

    2_1

---
+++ 2019_1_23/2019_1_23/main10.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//
+//int main()
+//{
+//	double sum = 0;
+//	for (int i = 0; i < 100; ++i) {
+//		sum += 1.0 * pow(-1,i) / (i + 1);
+//	}
+//	printf("%lf\n",sum);
+//	system("pause");
+//	return 0;
+//}
