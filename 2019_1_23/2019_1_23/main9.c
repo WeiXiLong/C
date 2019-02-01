ebf076efd27c5e0c0797162f638000e614766955
WeiXiLong <438545362@qq.com>
2019-02-01 10:18:41 +0800

    2_1

---
+++ 2019_1_23/2019_1_23/main9.c
@@ -0,0 +1,24 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int arr1[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int arr2[10] = { 10,9,8,7,6,5,4,3,2,1};
+//	int con = 0;
+//	for (int i = 0; i < 10; ++i) {
+//		con = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = con;
+//	}
+//	for (int i = 0; i < 10; ++i) {
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n");
+//	for (int i = 0; i < 10; ++i) {
+//		printf("%d ", arr2[i]);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
