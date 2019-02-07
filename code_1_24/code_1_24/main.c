e6ab706a6c19d306fc7a9538f8fbac34d2b71529
WeiXiLong <438545362@qq.com>
2019-02-07 13:19:14 +0800

    2_7

---
+++ code_1_24/code_1_24/main.c
@@ -0,0 +1,42 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main()
+//{
+//	int k = 6;
+//	int l = 13;
+//	int j = 1;
+//	int i = k;
+//	while (i != -1 && j != l + 1) {
+//		int tem = i;
+//		for (; tem > 0; --tem) {
+//			printf(" ");
+//		}
+//		int con = j;
+//		while(con > 0 ) {
+//			printf("*");
+//			--con;
+//		}
+//		printf("\n");
+//		j = j + 2;
+//		--i;
+//	}
+//	i = 1;
+//	j = l - 2;
+//	while ((i != k + 1 )&& j != 0) {
+//		for (int tem = 1; tem <= i; ++tem) {
+//			printf(" ");
+//		}
+//		int con = j;
+//		while (con > 0) {
+//			printf("*");
+//			--con;
+//		}
+//		printf("\n");
+//		j = j - 2;
+//		++i;
+//	}
+//	system("pause");
+//	return 0;
+//
+//}
