32741149546c4d8f4bfc4575b70bcc6a7ea5e72e
WeiXiLong <438545362@qq.com>
2019-02-07 13:19:59 +0800

    2——7

---
+++ code_1_24/code_1_24/main2.c
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//int main()
+//{
+//	for (int i = 0; i <= 999; ++i) {
+//		if (i / 10 == 0 ) { 
+//			printf("%d ",i);
+//		}
+//		else if ( (i / 10 > 0 ) && (i / 10 < 10 ) ) {
+//			if (pow(i / 10, 2) + pow(i % 10, 2) == i)
+//			{
+//				printf("%d ",i);
+//			}
+//		}
+//		else {
+//			if (pow(i / 100, 3) + pow(i / 10 % 10, 3) + pow(i % 10, 3) == i) {
+//				printf("%d ", i);
+//			}
+//		}
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
