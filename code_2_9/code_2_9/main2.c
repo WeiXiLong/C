cec6334ce1800aa7d45023652256699a557dd0b7
WeiXiLong <438545362@qq.com>
2019-02-13 17:31:03 +0800

    2_13

---
+++ code_2_9/code_2_9/main2.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main() {
+//	int a, b, c, d;
+//	for ( a = 0; a <= 1; ++a) {
+//		for (b = 0; b <= 1; ++b) {
+//			for (c = 0; c <= 1; ++c) {
+//				for (d = 0; d <= 1; ++d) {
+//					if ((!a + c + d + !d) == 3 && (a + b + c + d == 1)) {
+//						printf("a = %d , b = %d, c = %d, d = %d\n",a,b,c,d);
+//					}
+//				}
+//			}
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
