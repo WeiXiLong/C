2fcbb0df3b7d8e4b806e730294ccbbd0dc2cb4ce
WeiXiLong <438545362@qq.com>
2019-02-16 09:34:25 +0800

    2_16

---
+++ code_2_9/code_2_9/main4.c
@@ -0,0 +1,21 @@
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int count_one_bits(unsigned int value) {
+//	int count = 0;
+//	int num = sizeof(unsigned int) * 8;
+//	for (int i = 0; i < num; ++i) {
+//		if  ( ((value >> i) & 1) == 1) {
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//int main() {
+//	unsigned int num = 0;
+//	printf("请输入一个正整数 :");
+//	scanf("%u",&num);
+//	printf("这个正整数中有%u个二进制1\n", count_one_bits(num));
+//	system("pause");
+//	return 0;
+//}
