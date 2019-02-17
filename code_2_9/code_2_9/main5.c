527369398934143c6d1f87ae6446757bd95a5183
WeiXiLong <438545362@qq.com>
2019-02-17 12:27:11 +0800

    2_17

---
+++ code_2_9/code_2_9/main5.c
@@ -0,0 +1,18 @@
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main() {
+//	int num = 0;
+//	printf("请输入一个数字 :");
+//	scanf("%d",&num);
+//	for (int i = 0; i < sizeof(int) * 8; i = i + 2) {
+//		printf("%d ",(num >> i) & 1);
+//	}
+//	printf("\n");
+//	for (int i = 1; i < sizeof(int) * 8; i = i + 2) {
+//		printf("%d ", (num >> i) & 1);
+//	}
+//	system("pause");
+//	return 0;
+//}
