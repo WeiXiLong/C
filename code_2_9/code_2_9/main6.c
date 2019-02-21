7c9fcc2abf253a3667560d56c003dcddccdc8fb4
WeiXiLong <438545362@qq.com>
2019-02-21 12:41:51 +0800

    2_21

---
+++ code_2_9/code_2_9/main6.c
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//int main() {
+//	int num1 = 1999;
+//	int num2 = 2299;
+//	int count = 0;
+//	for (int i = 0; i < sizeof(int) * 8; ++i) {
+//		if (( ( (num1 >> i) ^ (num2 >> i ) ) & 1) == 1) {
+//			count++;
+//		}
+//	}
+//	printf("%d\n",count);
+//	system("pause");
+//	return 0;
+//}
