15bcfd8190ecc0cf6119bc76a3e3672fe43ec931
WeiXiLong <438545362@qq.com>
2019-02-22 15:54:57 +0800

    2_22

---
+++ code_2_9/code_2_9/main7.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//unsigned int reverse_bit(unsigned int value) {
+//	unsigned int num = 0;
+//	int size = sizeof(unsigned int) * 8;
+//	int i = size - 1;
+//	for (int j = 0; i > 0,j < size; ++j, --i){
+//			num += ((value >> j) & 1) * (unsigned int)pow(2, i);
+//	}
+//	return num;
+//}
+//int main() {
+//	unsigned int num = 0;
+//	scanf("%u",&num);
+//	printf("输出结果为: %u\n", reverse_bit(num));
+//	system("pause");
+//	return 0;
+//}
