2414e10044575188f94ba83f1a70c6f54e88db7c
WeiXiLong <438545362@qq.com>
2019-02-23 12:29:22 +0800

    2_23

---
+++ code_2_9/code_2_9/main8.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//double Averge(int num1 , int num2) {
+//	double ave = 0;
+//	int con = 0;
+//	if (num1 >= num2) {
+//		con = num1 - num2;
+//		ave = num2 + con / 2.0;
+//	}
+//	else {
+//		con = num2 - num1;
+//		ave = num1 + con / 2.0;
+//	}
+//	return ave;
+//}
+//int main() {
+//
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d",&num1,&num2);
+//	printf("平均值为: %lf\n", Averge(num1,num2));
+//	system("pause");
+//	return 0;
+//}
