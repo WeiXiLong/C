d5f2934dfd2a1554464db918a7f2d53666cdce32
WeiXiLong <438545362@qq.com>
2019-03-27 08:49:16 +0800

    3_27

---
+++ code_1_27/code_1_27/main.c
@@ -0,0 +1,18 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//void reverse_string(char * string) {
+//	if (*(string + 1) == '\0') {
+//		return ;
+//	}
+//	reverse_string(++string);
+//	printf("%c ", *string);
+//}
+//
+//int main() {
+//	char arr[] = "afdgt";
+//	reverse_string(arr - 1);
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
